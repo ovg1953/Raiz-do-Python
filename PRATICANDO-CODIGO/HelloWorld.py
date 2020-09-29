61de65d8bbc02d44311122962730363a257126e4
ovg1953 <71378996+ovg1953@users.noreply.github.com>
2020-09-29 08:01:15 -0300

    preenchendo

---
+++ PRATICANDO-CODIGO/HelloWorld.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+HelloWorld.py
+Created on Mon Sep 28 09:43:05 2020
+
+@author: ovgalva
+"""
+
+# Demo file for Spyder Tutorial
+# Galvão da UESC
+def hello():
+    """Prints 
+    Hello World
+    and return none"""
+    print('Hello World')
+    
+# Main program starts here
+hello()
